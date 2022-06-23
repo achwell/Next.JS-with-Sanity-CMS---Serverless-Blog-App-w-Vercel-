5bf13f0324d379e7fa0f2485567dab369992be1d
Axel Wulff Sæther <axel.sather@soprasteria.com>
2022-06-23 17:00:30 +0200

    Etter Section 3: Starting with Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,42 @@
 import {Row, Col} from 'react-bootstrap'
-import PageLayout from "components/PageLayout"
-import AuthorIntro from "components/AuthorIntro"
-import CardItem from "components/CardItem"
-import CardListItem from "components/CardListItem"
+import PageLayout from 'components/PageLayout'
+import AuthorIntro from 'components/AuthorIntro'
+import CardItem from 'components/CardItem'
+import CardListItem from 'components/CardListItem'
 
-export default function Home() {
+import {getAllBlogs} from 'lib/api'
+
+export default function Home({blogs}) {
     return (
         <PageLayout>
             <AuthorIntro/>
             <hr/>
             <Row className="mb-5">
-                <Col md="10">
-                    <CardListItem/>
-                </Col>
-                <Col md="4">
-                    <CardItem/>
-                </Col>
+                {/*<Col md="10">*/}
+                {/*    <CardListItem/>*/}
+                {/*</Col>*/}
+                {blogs.map(blog =>
+                    <Col key={blog.slug} md="4">
+                        <CardItem blog={blog} link={{
+                            href: '/blogs/[slug]',
+                            as: `/blogs/${blog.slug}`
+                        }}/>
+                    </Col>
+                )}
             </Row>
         </PageLayout>
     )
+}
+
+// This function is called during the build (build time)
+// Provides props to your page
+// It will create static page
+export async function getStaticProps() {
+    const blogs = await getAllBlogs()
+    console.log(blogs)
+    return {
+        props: {
+            blogs
+        }
+    }
 }
