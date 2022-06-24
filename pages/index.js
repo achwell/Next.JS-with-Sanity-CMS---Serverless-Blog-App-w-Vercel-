8900e1c1dd30e15bd5fc5a5e481072514950c536
Axel Wulff Sæther <axel.sather@soprasteria.com>
2022-06-24 11:48:30 +0200

    Etter Section 5: Blog List View & Client fetching

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,40 @@
+import { useState } from 'react';
 import {Row, Col} from 'react-bootstrap'
 import PageLayout from 'components/PageLayout'
 import AuthorIntro from 'components/AuthorIntro'
 import CardItem from 'components/CardItem'
 import CardListItem from 'components/CardListItem'
-
+import FilteringMenu from 'components/FilteringMenu';
 import {getAllBlogs} from 'lib/api'
+import { useGetBlogs } from 'actions';
 
-export default function Home({blogs}) {
+export default function Home({blogs: initialData}) {
+    const [filter, setFilter] = useState({
+        view: { list: 0 }
+    });
+
+    const { data: blogs, error } = useGetBlogs(initialData);
+
     return (
         <PageLayout>
             <AuthorIntro/>
+            <FilteringMenu
+                filter={filter}
+                onChange={(option, value) => setFilter({...filter, [option]: value})}
+            />
             <hr/>
             <Row className="mb-5">
-                {/*<Col md="10">*/}
-                {/*    <CardListItem/>*/}
-                {/*</Col>*/}
-                {blogs.map(blog =>
-                    <Col key={blog.slug} md="4">
-                        <CardItem blog={blog} link={{
-                            href: '/blogs/[slug]',
-                            as: `/blogs/${blog.slug}`
-                        }}/>
-                    </Col>
+                { blogs.map(blog => {
+                    let link = {href: '/blogs/[slug]', as: `/blogs/${blog.slug}`};
+                    return filter.view.list ?
+                            <Col md="9" key={`${blog.slug}-list`}>
+                                <CardListItem blog={blog} link={link}/>
+                            </Col>
+                            :
+                            <Col key={blog.slug} md="4">
+                                <CardItem blog={blog} link={link}/>
+                            </Col>;
+                    }
                 )}
             </Row>
         </PageLayout>
